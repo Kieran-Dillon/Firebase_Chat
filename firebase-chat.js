3a9d4e937532bba2778c921dad0a22e4eed7cc8e
Kieran Dillon <93695762+Kieran-Dillon@users.noreply.github.com>
2022-10-30 10:11:34 -0400

    Add files via upload

---
+++ firebase-chat.js
@@ -0,0 +1,140 @@
+(function () {
+  // TODO: replace this with your own firebase config object after creating app in your firebase console
+  // Your web app's Firebase configuration
+  // note: this config will not work indefinitely, will need to replace with your own config to run solution file
+  const firebaseConfig = {
+    apiKey: "AIzaSyAE8ewoiEZlTXJyLhlc8BucG4I3ztMXFtg",
+    authDomain: "fir-auth-1a573.firebaseapp.com",
+    projectId: "fir-auth-1a573",
+    storageBucket: "fir-auth-1a573.appspot.com",
+    messagingSenderId: "487849138116",
+    appId: "1:487849138116:web:7c22b570e7a8910a97b691"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  // handle on firebase db
+  const db = firebase.database();
+
+  // get elements
+  const email = document.getElementById('email');
+  const password = document.getElementById('password');
+  const login = document.getElementById('login');
+  const signup = document.getElementById('signup');
+  const logout = document.getElementById('logout');
+  const message = document.getElementById('message');
+  const write = document.getElementById('write');
+  const read = document.getElementById('read');
+  const status = document.getElementById('status');
+
+  // div to hold contents of messages db as a chat box between any users
+  const chat = document.getElementById('chat-box');
+
+  // element that should show the currently logged in user's email address
+  const userNameDisplay = document.getElementById('name-display');
+  let currentUserEmail = ''; // variable to store the current user's email
+
+  // write
+  write.addEventListener('click', (e) => {
+    const messages = db.ref('messages');
+
+    // write to db
+    messages
+      .child(id)
+      .set({ message: message.value, sender: currentUserEmail })
+      .then(function () {
+        console.log('Wrote to DB!');
+      });
+  });
+
+  // read
+  read.addEventListener('click', (e) => {
+    handleRead();
+  });
+
+  //messagesRef to listen for updates and update the chat div on any update, not just when the 'Update Chat' button is clicked
+  const messagesRef = db.ref('messages');
+  messagesRef.on('value', (snapshot) => {
+    handleRead();
+  });
+
+  // Function triggered on click of the 'Update Chat' button which parses the contents of the messages array to show them in the chat window
+  function handleRead() {
+    status.innerHTML = '';
+    chat.innerHTML = '';
+    const messages = db.ref('messages');
+
+    messages.once('value').then(function (dataSnapshot) {
+      var data = dataSnapshot.val();
+      if (data) {
+        var keys = Object.keys(data);
+
+        keys.forEach(function (key) {
+          console.log(data[key]);
+          chat.innerHTML +=
+            (data[key]['sender'] || '') +
+            '   :   ' +
+            data[key].message +
+            '<br><br>';
+        });
+      }
+    });
+  }
+
+  function updateCurrentUser(userEmail) {
+    userNameDisplay.innerHTML = userEmail;
+    currentUserEmail = userEmail;
+  }
+
+  // login
+  login.addEventListener('click', (e) => {
+    const auth = firebase.auth();
+    const promise = auth.signInWithEmailAndPassword(
+      email.value,
+      password.value
+    );
+    promise.then((resp) => {
+      console.log('User Login Response: ', resp);
+      logout.style.display = 'inline';
+      login.style.display = 'none';
+      signup.style.display = 'none';
+      write.style.display = 'inline';
+      updateCurrentUser(resp.user.email);
+    });
+    promise.catch((e) => console.log(e.message));
+  });
+
+  // signup
+  signup.addEventListener('click', (e) => {
+    const auth = firebase.auth();
+    const promise = auth.createUserWithEmailAndPassword(
+      email.value,
+      password.value
+    );
+    promise.then((resp) => {
+      console.log('User Signup + Login Response: ', resp);
+      logout.style.display = 'inline';
+      login.style.display = 'none';
+      signup.style.display = 'none';
+      write.style.display = 'inline';
+      updateCurrentUser(resp.user.email);
+    });
+    promise.catch((e) => console.log(e.message));
+  });
+
+  // logout
+  logout.addEventListener('click', (e) => {
+    firebase
+      .auth()
+      .signOut()
+      .then((resp) => {
+        console.log('Logout Response: ', resp);
+        logout.style.display = 'none';
+        login.style.display = 'inline';
+        signup.style.display = 'inline';
+        write.style.display = 'none';
+        updateCurrentUser('');
+      })
+      .catch((e) => console.warn(e.message));
+  });
+})();
